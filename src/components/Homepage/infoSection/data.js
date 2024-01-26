08ce68b6e8d99a9b34730b67840df9022e6c40cc
Joshua Barua <joshuabarua9@hotmail.com>
2024-01-26 17:34:19 +0100

    Fix homepage

--- src/components/Homepage/infoSection/data.js
+++ src/components/Homepage/infoSection/data.js
@@ -1,17 +1,72 @@
+import filmtv from '../../../assets/images/services-icons/filmtv.png';
+import commercials from '../../../assets/images/services-icons/commercials.png';
+import theatre from '../../../assets/images/services-icons/theatre.png';
+import workshops from '../../../assets/images/services-icons/workshops.png';
+import fashion from '../../../assets/images/services-icons/fashion.png';
+import bespoke from '../../../assets/images/services-icons/bespoke.png';
+import emImg2 from '../../../assets/images/em2.jpg';
+import emImg3 from '../../../assets/images/em1.jpg';
+
 export const aboutObjOne = {
-	id: "about",
+	id: 'about',
 	lightBg: false,
 	lightText: true,
 	lightTextDesc: true,
-	topLine: "About Me",
-	headline: `Hi! My name is Emma, and I'm a Makeup and SFX artist.`,
+	topLine: 'About Me',
+	headline: `Welcome! I'm Emma, your go-to Makeup and Special Effects (SFX) virtuoso.`,
 	description:
-		"I have 5 years of experience specialising in beauty and media makeup. I've trained in barbering, film and tv. Have a look at my portfolio and CV. If you're interested in working together, don't forget to send me a message!",
-	buttonLabel: "Portfolio",
-	imgStart: false,
-	imgEm: "../../assets/images/em2.jpg",
-	alt: "Emma Barua",
+		"With a vibrant journey of over 5 years, I've honed my craft in the realms of beauty and media makeup, leaving a trail of awe-inspiring transformations. My expertise isn't just skin deep; it's rooted in comprehensive training that spans barbering, film, and television, ensuring every brushstroke tells a story. I invite you to immerse yourself in my meticulously curated portfolio, a testament to my dedication and artistic flair. While you're there, don't miss my CV, a chronicle of my professional odyssey. Eager to bring your vision to life? Let's connect! Drop me a message, and let's sculpt the extraordinary together.",
+	buttonLabel: 'Explore My Artistry',
+	imgStart: true,
+	imgEm: emImg2,
+	alt: 'Emma Barua',
 	dark: true,
 	primary: true,
 	darkText: false,
+};
+
+export const servicesObj = {
+	id: 'services',
+	lightBg: true,
+	lightText: false,
+	lightTextDesc: false,
+	topLine: 'Services',
+	headline: `Discover the Magic of Makeup Artistry`,
+	services: {
+		filmtv: {
+			description:
+				'Film and TV: From Netflix dramas to ITV’s live segments, I bring narratives to life with my diverse skill set, adapting from subtle enhancements to vibrant transformations.',
+			icon: filmtv,
+		},
+		commercial: {
+			description:
+				'Commercial and Advertising: Partner with me for campaigns that resonate. My portfolio includes major brands like Jacob’s Mini Cheddars and Amazon Web Services, ensuring your message is impactful and memorable.',
+			icon: commercials,
+		},
+		theatre: {
+			description:
+				"Theatre and Performance: Experience the magic of theatre and performance makeup. Whether it's for a local play or a high-profile fashion show, I capture the essence of each character with meticulous detail.",
+			icon: theatre,
+		},
+		workshops: {
+			description:
+				'Workshops and Training: Join my transformative workshops where I share my extensive knowledge, from SFX casualty makeup to intensive training sessions, nurturing the next generation of makeup artists.',
+			icon: workshops,
+		},
+		fashion: {
+			description:
+				'Fashion and Editorial: Elevate your fashion projects with my creative flair. From fashion shows to editorial shoots, my work ensures your project is the epitome of style and sophistication.',
+			icon: fashion,
+		},
+		bespoke: {
+			description:
+				'Bespoke Services: Tailored for your special occasions, from weddings to events, I provide personalized makeup services that ensure you look and feel your absolute best.',
+			icon: bespoke,
+		},
+	},
+	imgStart: false,
+	alt: 'Emma Robyn at Work',
+	dark: false,
+	primary: false,
+	darkText: true,
 };
--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,61 +1,40 @@
-import React from "react";
-import {Icon} from "@iconify/react";
-import "./sliderStyles.css";
-import perfect10 from "../../assets/images/logos-filmtitles/Perfect10film.jpeg";
-import Amw from "../../assets/images/logos-filmtitles/auroraMediaWorldwide.jpg";
-import Itv from "../../assets/images/logos-filmtitles/itv-logo.png";
-import Baerclaw from "../../assets/images/logos-filmtitles/baerclawproductions.png";
-import Goodwood from "../../assets/images/logos-filmtitles/goodwood.png";
-import Underglass from "../../assets/images/logos-filmtitles/underglass.png";
-import Jacobsminis from "../../assets/images/logos-filmtitles/jacobsminis.png";
+import React from 'react';
+import {Icon} from '@iconify/react';
+import './sliderStyles.css';
+import perfect10 from '../../assets/images/logos-filmtitles/Perfect10film_resized.jpeg';
+import Amw from '../../assets/images/logos-filmtitles/auroraMediaWorldwide.jpg';
+import Itv from '../../assets/images/logos-filmtitles/itv-hub-100.png';
+import Aws from '../../assets/images/logos-filmtitles/aws-100.png';
+import Imdb from '../../assets/images/logos-filmtitles/imdb-100.png';
+import Netflix from '../../assets/images/logos-filmtitles/netflix-100.png';
+import Baerclaw from '../../assets/images/logos-filmtitles/baerclawproductions_enhanced.png';
+import Goodwood from '../../assets/images/logos-filmtitles/goodwood_enhanced.png';
+import Underglass from '../../assets/images/logos-filmtitles/underglass_enhanced.png';
+import Jacobsminis from '../../assets/images/logos-filmtitles/jacobsminis_resized.png';
+import Marquee from 'react-fast-marquee';
 
 const Slider = () => {
 	const companies = [
-		{logo: Underglass, className: "logosImg4"},
-		{logo: Jacobsminis, className: "logosImg3"},
-		{logo: "logos:netflix", className: "logosImg", iconSize: "50px"},
-		{logo: Goodwood, className: "logosImg2"},
-		{logo: "fa-brands:imdb", className: "logosImg2", iconSize: "100px"},
-		{logo: perfect10, className: "logosImg2"},
-		{logo: "logos:aws", className: "logosImg2", iconSize: "100px"},
-		{logo: Amw, className: "logosImg2"},
-		{logo: Baerclaw, className: "logosImg2"},
-		{logo: Itv, className: "logosImg3"},
-		{logo: Underglass, className: "logosImg4"},
-		{logo: Jacobsminis, className: "logosImg3"},
-		{logo: "logos:netflix", className: "logosImg", iconSize: "50px"},
-		{logo: Goodwood, className: "logosImg2"},
-		{logo: "fa-brands:imdb", className: "logosImg2", iconSize: "100px"},
-		{logo: perfect10, className: "logosImg2"},
-		{logo: "logos:aws", className: "logosImg2", iconSize: "100px"},
-		{logo: Amw, className: "logosImg2"},
-		{logo: Baerclaw, className: "logosImg2"},
-		{logo: Itv, className: "logosImg3"},
+		{logo: Underglass, className: 'logosImg4'},
+		{logo: Jacobsminis, className: 'logosImg3'},
+		{logo: Imdb, className: 'logosImg2'},
+		{logo: Goodwood, className: 'logosImg2'},
+		{logo: Netflix, className: 'logosImg2'},
+		{logo: perfect10, className: 'logosImg2'},
+		{logo: Itv, className: 'logosImg2'},
+		{logo: Amw, className: 'logosImg2'},
+		{logo: Baerclaw, className: 'logosImg2'},
+		{logo: Aws, className: 'logosImg2'},
 	];
-	//TODO: FIX THIS SLIDER!!
+
 	return (
-		<>
-			<div className="sliderContainer">
-				<div className="slider">
-					<ul>
-						{companies.map((company, index) => (
-							<li key={index}>
-								<div className={company.className}>
-									{company.iconSize ? (
-										<Icon
-											icon={company.logo}
-											style={{fontSize: company.iconSize || "inherit"}}
-										/>
-									) : (
-										<img src={company.logo} alt="" />
-									)}
-								</div>
-							</li>
-						))}
-					</ul>
-				</div>
-			</div>
-		</>
+		<div className="marquee-container">
+			<Marquee speed={70} gradient={true}>
+				{companies.map((company, index) => {
+					return <img key={index} src={company.logo} className={`${company.className} marquee-logo`} style={{margin: '0 30px'}} alt="company-logo" />;
+				})}
+			</Marquee>
+		</div>
 	);
 };
 
