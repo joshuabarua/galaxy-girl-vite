7ec028f12a3c40ffa25a0cf582635e87f0bd25b7
Joshua Barua <joshuabarua9@hotmail.com>
2024-01-27 15:37:37 +0100

    Home page  hero section and dropdown style changes

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -25,38 +25,38 @@ const Navbar = ({toggle, scrollHeight = 800, initialBackground = 'rgba(36, 0, 59
 	const background = location.pathname === '/' ? (scrollNav ? scrolledBackground : initialBackground) : scrolledBackground;
 
 	return (
-		<>
-			<nav className="nav" style={{background}}>
+		<nav className="nav" style={{background}}>
+			<div className="nav-logo">
 				<LinkR className="nav-link" to="/" style={{color: scrollNav ? 'transparent' : '#ddd8ff'}}>
 					<img src={logoImg} className="logo-img" alt="logo" />
 				</LinkR>
-				<FaBars className="bars" onClick={toggle} />
+			</div>
+			<FaBars className="bars" onClick={toggle} />
 
-				<div className="nav-menu">
-					<LinkR className="nav-link" to="/" exact="true" strict="true">
-						Home
-					</LinkR>
-					<LinkR className="nav-link" to="/resume">
-						CV
-					</LinkR>
-					<LinkR className="nav-link" to="/portfolio">
-						Portfolio
-					</LinkR>
-					<LinkR className="nav-link" to="/contact">
-						Contact
-					</LinkR>
-				</div>
+			<div className="nav-menu">
+				<LinkR className="nav-link" to="/" exact="true" strict="true">
+					Home
+				</LinkR>
+				<LinkR className="nav-link" to="/resume">
+					CV
+				</LinkR>
+				<LinkR className="nav-link" to="/portfolio">
+					Portfolio
+				</LinkR>
+				<LinkR className="nav-link" to="/contact">
+					Contact
+				</LinkR>
+			</div>
 
-				<div className="nav-btn">
-					<a href="https://www.facebook.com/thegalaxygirl" target="_blank" rel="noreferrer">
-						<FaFacebook className="icon-fb" />
-					</a>
-					<a href="https://www.instagram.com/emmathegalaxygirl/" target="_blank" rel="noreferrer">
-						<FaInstagram className="icon-insta" />
-					</a>
-				</div>
-			</nav>
-		</>
+			<div className="nav-btn">
+				<a href="https://www.facebook.com/thegalaxygirl" target="_blank" rel="noreferrer">
+					<FaFacebook className="icon-fb" />
+				</a>
+				<a href="https://www.instagram.com/emmathegalaxygirl/" target="_blank" rel="noreferrer">
+					<FaInstagram className="icon-insta" />
+				</a>
+			</div>
+		</nav>
 	);
 };
 
