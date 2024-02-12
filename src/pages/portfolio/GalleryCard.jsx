3c05acd881295c4eabd1e242b2445c7ea2d6fb1a
Joshua Barua <joshua.k.barua@gmail.com>
2024-02-12 16:01:16 +0100

    Styling portfolio and gallery cards

--- src/pages/portfolio/GalleryCard.jsx
+++ src/pages/portfolio/GalleryCard.jsx
@@ -4,14 +4,19 @@ import {NavLink, useParams} from 'react-router-dom';
 
 function GalleryCard({imageGroup, id}) {
 	const {galleryId} = useParams();
-	console.log('key', id);
+
+	//TODO: Add arrows >> next to navlink
 	return (
 		<div className={styles.gallery_card}>
 			<div className={styles.gallery_card_img}>
 				<img src={`${imageGroup.images[0].src}`} />
 			</div>
-			<h3>{imageGroup.name}</h3>
-			<NavLink to={`/portfolio/gallery/${id}`}> TO GALLERY </NavLink>
+			<div className={styles.card_text}>
+				<h2>{imageGroup.name}</h2>
+				<NavLink to={`/portfolio/gallery/${id}`} className={styles.gallery_link}>
+					TO GALLERY
+				</NavLink>
+			</div>
 		</div>
 	);
 }
