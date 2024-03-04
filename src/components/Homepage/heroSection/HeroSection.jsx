8ffe082fc0e9ea7242c91cd6f16802473644f0bc
Joshua Barua <joshua.k.barua@gmail.com>
2024-03-04 13:17:49 +0100

    classname

--- src/components/Homepage/heroSection/HeroSection.jsx
+++ src/components/Homepage/heroSection/HeroSection.jsx
@@ -1,61 +1,48 @@
-import React, { useState, useEffect, useRef } from "react";
-import "./heroSectionStyles.css"; // Import the CSS file
-import Video from "../../../assets/videos/video4.mp4";
-import { Button } from "../../../pages/buttonElement";
-import { MdKeyboardArrowRight, MdArrowForward } from "react-icons/md";
+import React, {useState, useEffect, useRef} from 'react';
+import './heroSectionStyles.css'; // Import the CSS file
+import Video from '../../../assets/videos/video4.mp4';
+import {Button} from '../../../pages/buttonElement';
+import {MdKeyboardArrowRight, MdArrowForward} from 'react-icons/md';
 
 const HeroSection = () => {
-  const [hover, setHover] = useState(false);
-  const vidRef = useRef();
-  useEffect(() => {
-    vidRef.current.play();
-  }, []);
+	const [hover, setHover] = useState(false);
+	const vidRef = useRef();
+	useEffect(() => {
+		vidRef.current.play();
+	}, []);
 
-  const onHover = () => {
-    setHover(!hover);
-  };
+	const onHover = () => {
+		setHover(!hover);
+	};
 
-  return (
-    <div className="hero-container" id="home">
-      <div className="hero-bg">
-        <video
-          className="video-bg"
-          playsInline
-          ref={vidRef}
-          muted
-          autoPlay={true}
-          loop
-          src={Video}
-          disablePictureInPicture={true}
-          disableRemotePlayback={true}
-          type="video/mp4"
-        />
-      </div>
-      <div className="hero-content">
-        <h1 className="hero-h1"> Emma Robyn </h1>
-        <h1>SFX Makeup Artist</h1>
-        <h2 className="hero-p">
-          Everything has beauty in it, but not everyone sees it - Confucius
-        </h2>
-        <div className="hero-btn-wrapper">
-          <Button
-            to="/portfolio"
-            onMouseEnter={onHover}
-            onMouseLeave={onHover}
-            primary="true"
-            dark="true"
-          >
-            Explore My Artistry{" "}
-            {hover ? (
-              <MdArrowForward className="arrow-forward" />
-            ) : (
-              <MdKeyboardArrowRight className="arrow-right" />
-            )}
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="hero-container" id="home">
+			<div className="hero-bg">
+				<video
+					className="video-bg"
+					playsInline
+					ref={vidRef}
+					muted
+					autoPlay={true}
+					loop
+					src={Video}
+					disablePictureInPicture={true}
+					disableRemotePlayback={true}
+					type="video/mp4"
+				/>
+			</div>
+			<div className="hero-content">
+				<h1 className="hero-h1"> Emma Robyn </h1>
+				<h1 className="hero-h1">SFX Makeup Artist</h1>
+				<h2 className="hero-p">Everything has beauty in it, but not everyone sees it - Confucius</h2>
+				<div className="hero-btn-wrapper">
+					<Button to="/portfolio" onMouseEnter={onHover} onMouseLeave={onHover} primary="true" dark="true">
+						Explore My Artistry {hover ? <MdArrowForward className="arrow-forward" /> : <MdKeyboardArrowRight className="arrow-right" />}
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default HeroSection;
