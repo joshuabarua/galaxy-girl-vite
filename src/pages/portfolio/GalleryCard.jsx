3e931c802ddfd0c932ff9c97aebdb7ecee10ce7c
Joshua Barua <joshua.k.barua@gmail.com>
2024-03-12 10:17:40 +0100

    Remove p5, make homepage images smaller

--- src/pages/portfolio/GalleryCard.jsx
+++ src/pages/portfolio/GalleryCard.jsx
@@ -6,57 +6,57 @@ import p5 from 'p5';
 function GalleryCard({imageGroup, id}) {
 	const navigate = useNavigate();
 
-	const sketchRef = useRef();
-	let p5Instance = useRef(null);
+	// const sketchRef = useRef();
+	// let p5Instance = useRef(null);
 
-	useEffect(() => {
-		p5Instance.current = new p5((p) => {
-			let img;
+	// useEffect(() => {
+	// 	p5Instance.current = new p5((p) => {
+	// 		let img;
 
-			p.preload = () => {
-				img = p.loadImage(imageGroup.images[0].src);
-			};
-			p.setup = () => {
-				p.frameRate(10);
-				let canvas = p.createCanvas(1024, 780);
-				p.image(img, -1000, -1000);
-				canvas.mouseOver(pauseDrawing);
-				canvas.mouseOut(resumeDrawing);
-			};
+	// 		p.preload = () => {
+	// 			img = p.loadImage(imageGroup.images[0].src);
+	// 		};
+	// 		p.setup = () => {
+	// 			p.frameRate(10);
+	// 			let canvas = p.createCanvas(1024, 780);
+	// 			p.image(img, -1000, -1000);
+	// 			canvas.mouseOver(pauseDrawing);
+	// 			canvas.mouseOut(resumeDrawing);
+	// 		};
 
-			p.draw = () => {
-				for (let x = 0; x < p.width; x += 7) {
-					let offset = p.sin(p.frameCount * 0.05 + x * 0.02) * 10;
+	// 		p.draw = () => {
+	// 			for (let x = 0; x < p.width; x += 7) {
+	// 				let offset = p.sin(p.frameCount * 0.05 + x * 0.02) * 10;
 
-					let sx = x % img.width;
-					let sy = 0;
-					let sw = 5;
-					let sh = img.height;
+	// 				let sx = x % img.width;
+	// 				let sy = 0;
+	// 				let sw = 5;
+	// 				let sh = img.height;
 
-					let dx = x + offset;
-					let dy = 0;
-					let dw = 10;
-					let dh = p.height;
+	// 				let dx = x + offset;
+	// 				let dy = 0;
+	// 				let dw = 10;
+	// 				let dh = p.height;
 
-					p.copy(img, sx, sy, sw, sh, dx, dy, dw, dh);
-				}
-			};
+	// 				p.copy(img, sx, sy, sw, sh, dx, dy, dw, dh);
+	// 			}
+	// 		};
 
-			function pauseDrawing() {
-				p.noLoop();
-			}
+	// 		function pauseDrawing() {
+	// 			p.noLoop();
+	// 		}
 
-			function resumeDrawing() {
-				p.loop();
-			}
-		}, sketchRef.current);
+	// 		function resumeDrawing() {
+	// 			p.loop();
+	// 		}
+	// 	}, sketchRef.current);
 
-		return () => {
-			if (p5Instance.current) {
-				p5Instance.current.remove();
-			}
-		};
-	}, [imageGroup.images[0].src]);
+	// 	return () => {
+	// 		if (p5Instance.current) {
+	// 			p5Instance.current.remove();
+	// 		}
+	// 	};
+	// }, [imageGroup.images[0].src]);
 
 	const handleClick = () => {
 		navigate(`/portfolio/gallery/${id}`);
@@ -64,8 +64,8 @@ function GalleryCard({imageGroup, id}) {
 	return (
 		<div className={styles.gallery_card} onClick={handleClick}>
 			<div className={styles.gallery_card_img}>
-				<div ref={sketchRef} className={styles.galleryCardImgDiv} />
-				{/* <img src={`${imageGroup.images[0].src}`} /> */}
+				{/* <div ref={sketchRef} className={styles.galleryCardImgDiv} /> */}
+				<img src={`${imageGroup.images[0].src}`} />
 			</div>
 			<div className={styles.card_text}>
 				<h2>{imageGroup.name}</h2>
