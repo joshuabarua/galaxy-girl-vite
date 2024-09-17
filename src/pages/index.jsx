1aa47c501ed56cb37477b906f2f12a5201549acd
Joshua Barua <joshuabarua9@hotmail.com>
2024-09-17 19:17:23 +0200

    Remove non tailwind classes, Make files tailwind and remove fluff

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import HeroSection from '../components/Homepage/heroSection/HeroSection';
 import InfoSection from '../components/Homepage/infoSection/InfoSection';
 import Slider from '../components/slider/Slider';
@@ -7,11 +7,19 @@ import ServicesSection from '../components/Homepage/servicesSection/ServicesSect
 import gsap from 'gsap';
 import {useGSAP} from '@gsap/react';
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
+import {useStore} from '@/hooks/useStore';
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const Home = () => {
+	const ready = useStore((state) => state.ready);
+	const isReady = useStore((state) => state.isReady);
+	const vidRef = useRef();
+
 	useGSAP(() => {
+		if (!vidRef.current) return;
+
+		isReady();
 		const introTimeline = gsap.timeline();
 
 		introTimeline
@@ -71,27 +79,43 @@ const Home = () => {
 				'.top-line',
 				{
 					scale: 1.3,
-					y: 20,
+					y: 30,
 					opacity: 0,
 					rotate: -10,
 					ease: 'back.out(3)',
 					duration: 0.3,
 				},
-				0.9
+				0.2
 			)
-			.from('.heading', {
-				y: 20,
-				opacity: 0,
-			})
-			.from('.subtitle', {
-				y: 20,
-				opacity: 0,
-			});
-	});
+			.from(
+				'.heading',
+				{
+					y: 30,
+					opacity: 0,
+				},
+				0.4
+			)
+			.from(
+				'.subtitle',
+				{
+					y: 30,
+					opacity: 0,
+				},
+				0.5
+			)
+			.from(
+				'.img-em',
+				{
+					opacity: 0,
+				},
+				0.5
+			);
+	}),
+		{dependencies: [ready]};
 
 	return (
 		<div className="home">
-			<HeroSection />
+			<HeroSection vidRef={vidRef} />
 			<InfoSection {...aboutObjOne} />
 			<Slider />
 			<ServicesSection />
