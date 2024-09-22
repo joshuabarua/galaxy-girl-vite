5e237ebb7158375182d0610270a1b858bca436b5
Joshua Barua <joshua.k.barua@gmail.com>
2024-09-22 18:35:23 +0200

    Update vercel, change browserrouter to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import {useState, useEffect} from 'react';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
+import {useState} from 'react';
 import './App.css';
 import Home from './pages/index';
 import Resume from './pages/resume/index';
